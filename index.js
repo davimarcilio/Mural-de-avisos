98fe81715e4f26862a66b8f79039d8d7cafefb0d
Davi Marcilio <davimarcilio.js@gmail.com>
2022-08-27 13:02:36 -0300

    testes

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const posts = require('./model/posts');
-
-app.get('/all', (req, res) => {
-    res.json(JSON.stringify(posts.getAll()))
-})
-app.post('/new', bodyParser.json(), (req, res) => {
-    let title = req.body.title;
-    let description = req.body.description;
-    posts.newPost(title, description);
-
-    res.send('Post Adicionado');
-})
+const apiRoute = require('./routes/api');
+const path = require('path');
+app.use('/api', apiRoute);
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
     console.log('Porta Aberta: ', PORT);
 })
---
+++ public/scripts/script.js
@@ -0,0 +1,6 @@
+function updatePosts() {
+
+}
+function newPost() {
+
+}
